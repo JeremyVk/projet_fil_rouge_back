6eb32722df4e4f9d9e27d6adc6404af0ae68a7a1
JeremyVk <jeremvk@outlook.fr>
2022-09-30 10:25:24 +0200

    add book fixtures

---
+++ src/Fixtures/BookFixture.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Fixtures;
+
+use App\Entity\Book;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class BookFixture extends Fixture
+{
+    const BOOKS = [
+        [
+            'title' =>  'Apprendre le HTML',
+            'resume' => "Un classique du HTML",
+            'isbnNumber' => 15786896,
+            'format' => 'brochet',
+            'editor' => 'Lys Bleu',
+            'author' => 'cyberTech',
+            'gender' => 'Apprentissage Informatique',
+            'unitPrice' => 4000,
+            'stock' => 10,
+        ],
+        [
+            'title' =>   'Apprendre le CSS',
+            'resume' => "Faites de beaux sites avec le CSS",
+            'isbnNumber' => 15786821,
+            'format' => 'Poche',
+            'editor' => 'Hachette',
+            'author' => 'cssLoveur',
+            'gender' => 'Apprentissage Informatique',
+            'unitPrice' => 4000,
+            'stock' => 10,
+        ],
+        [
+            'title' =>    'Le javascript en 5 étapes',
+            'resume' => "Les étapes clef pour apprendre le javascript",
+            'isbnNumber' => 1578612,
+            'format' => 'Poche',
+            'editor' => 'Aparis',
+            'author' => 'JsPlayer',
+            'gender' => 'Apprentissage Informatique',
+            'unitPrice' => 3500,
+            'stock' => 10,
+        ],
+    ];
+
+
+    public function load(ObjectManager $manager)
+    {
+        foreach(SELF::BOOKS as $dataBook) {
+            $book = new Book();
+            $book->setTitle($dataBook['title']);
+            $book->setResume($dataBook['resume']);
+            $book->setUnitPrice($dataBook['unitPrice']);
+            $book->setStock($dataBook['stock']);
+            $book->setImage('image/image');
+            $book->setIsbnNumber($dataBook['isbnNumber']);
+            $book->setFormat($dataBook['format']);
+            $book->setEditor($dataBook['editor']);
+
+            $manager->persist($book);
+
+        }
+        $manager->flush();
+    }
+
+
+}
