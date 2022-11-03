2e423b3ca06cf2f4d60d544e9841cd0cfd32eb58
JeremyVk <jeremvk@outlook.fr>
2022-11-03 15:43:47 +0100

    upgrade of search book with the search bar

---
+++ src/Filters/AllBookSearchFilter.php
@@ -0,0 +1,56 @@
+<?php
+// api/src/Filter/RegexpFilter.php
+
+namespace App\Filters;
+
+use ApiPlatform\Doctrine\Orm\Filter\AbstractFilter;
+use ApiPlatform\Doctrine\Orm\Util\QueryNameGeneratorInterface;
+use ApiPlatform\Metadata\Operation;
+use Doctrine\ORM\QueryBuilder;
+use Symfony\Component\PropertyInfo\Type;
+
+final class AllBookSearchFilter extends AbstractFilter
+{
+
+    protected function filterProperty(string $property, $value, QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, Operation $operation = null, array $context = []): void
+    {
+        $querys = explode(" ", $value);
+
+        foreach($querys as $query) {
+            $queryBuilder  
+            ->orWhere("o.title LIKE :value")
+            ->orWhere("o.editor LIKE :value")
+            ->orWhere("o.resume LIKE :value")
+            ->setParameter('value', "%" . $query . "%");  
+        } 
+
+        $queryBuilder
+        ->orderBy("o.stock", "DESC");
+}
+
+    // This function is only used to hook in documentation generators (supported by Swagger and Hydra)
+    public function getDescription(string $resourceClass): array
+    {
+        if (!$this->properties) {
+            return [];
+        }
+
+        $description = [];
+        foreach ($this->properties as $property => $strategy) {
+            $description["regexp_$property"] = [
+                'property' => $property,
+                'type' => Type::BUILTIN_TYPE_STRING,
+                'required' => false,
+                'description' => 'Filter using a regex. This will appear in the OpenApi documentation!',
+                'openapi' => [
+                    'example' => 'Custom example that will be in the documentation and be the default value of the sandbox',
+                    'allowReserved' => false,// if true, query parameters will be not percent-encoded
+                    'allowEmptyValue' => true,
+                    'explode' => false, // to be true, the type must be Type::BUILTIN_TYPE_ARRAY, ?product=blue,green will be ?product=blue&product=green
+                ],
+            ];
+        }
+
+        return $description;
+    }
+}
