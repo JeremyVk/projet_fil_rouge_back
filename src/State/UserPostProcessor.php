8c309002051e217a9633d0ac060c37b097a8f7c9
JeremyVk <jeremvk@outlook.fr>
2023-02-23 08:04:27 +0100

    start the user password change

--- src/State/UserPostProcessor.php
+++ src/State/UserPostProcessor.php
@@ -3,6 +3,7 @@
 namespace App\State;
 
 use ApiPlatform\Metadata\Operation;
+use ApiPlatform\Metadata\Put;
 use ApiPlatform\State\ProcessorInterface;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
@@ -13,12 +14,28 @@ class UserPostProcessor implements ProcessorInterface
     {
         
     }
-    public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = []): void
+    public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = [])
     {
-        // Handle the state
-        $data->setPassword($this->passwordHasher->hashPassword($data, $data->getPassword()));
-        $this->em->persist($data);
-        $this->em->flush();
-        // dd($data);
+        dd($data);
+        if ($operation instanceof Put) {
+            // $hashedPassword = $this->passwordHasher->hashPassword($data, $data->getPlainPassword());
+            // dd($data, $hashedPassword);
+            dd($this->passwordHasher->isPasswordValid($data, $data->getPlainPassword()));
+        }
+        if (!$data->getPlainPassword()) {
+            return $this->em->flush($data);
+        }
+
+        if ($data->getPlainPassword()) {
+            $hashedPassword = $this->passwordHasher->hashPassword($data, $data->getPlainPassword());
+            $data->setPassword($hashedPassword);
+            $this->em->persist($data);
+            return $this->em->flush();
+        }
+
+        $hashedPassword = $this->passwordHasher->hashPassword(
+            $data,
+            $data->getPlainPassword()
+        );
     }
 }
