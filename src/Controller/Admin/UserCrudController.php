356b63b2b3f984d4ebd58d1554ef347adb308d2b
JeremyVk <jeremvk@outlook.fr>
2023-03-13 11:57:01 +0100

    add admin user post event for hash password

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -5,12 +5,26 @@ namespace App\Controller\Admin;
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Dto\EntityDto;
+use EasyCorp\Bundle\EasyAdminBundle\Config\KeyValueStore;
+use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
 
 class UserCrudController extends AbstractCrudController
 {
+
+    public function __construct(
+        private UserPasswordHasherInterface $userPasswordHasher
+    )
+    {   
+    }
+
     public static function getEntityFqcn(): string
     {
         return User::class;
@@ -21,10 +35,45 @@ class UserCrudController extends AbstractCrudController
         return [
             IdField::new('id')->onlyOnIndex(),
             TextField::new('email'),
-            TextField::new('password'),
+            TextField::new('plainPassword')->setFormType(PasswordType::class)->setFormTypeOptions([
+                'mapped' => true,
+                'label' => "Password"
+            ])
+            ->setRequired($pageName === Crud::PAGE_NEW)
+            ->onlyOnForms(),
             TextField::new('firstName'),
             TextField::new('lastName'),
-            AssociationField::new('addresses'),
+            AssociationField::new('addresses')
+            ->setFormTypeOptions([
+                'label' => 'Adresses'
+            ]),
         ];
+    }
+
+    public function createEditFormBuilder(EntityDto $entityDto, KeyValueStore $formOptions, AdminContext $context): FormBuilderInterface
+    {
+        $formBuilder = parent::createEditFormBuilder($entityDto, $formOptions, $context);
+        return $this->addPasswordEventListener($formBuilder);
+    }
+
+    private function addPasswordEventListener(FormBuilderInterface $formBuilder): FormBuilderInterface
+    {
+        return $formBuilder->addEventListener(FormEvents::POST_SUBMIT, $this->hashPassword());
+    }
+
+    private function hashPassword() {
+        return function($event) {
+            $form = $event->getForm();
+            if (!$form->isValid()) {
+                return;
+            }
+            $password = $form->get('plainPassword')->getData();
+            if ($password === null) {
+                return;
+            }
+
+            $hash = $this->userPasswordHasher->hashPassword($form->getData(), $password);
+            $form->getData()->setPassword($hash);
+        };
     }
 }
